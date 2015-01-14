547bdd5a8a13290cd71b57b943c0612267a64549
Pushpendre Rastogi <pushpendre@gmail.com>
2015-01-14 18:29:51 -0500

    Prepare for removing E2LSH into its own little package MILSH.

--- E2LSH-0.1/sources/setup.py
+++ E2LSH-0.1/sources/setup.py
@@ -1,13 +1,18 @@
-# python setup.py build_ext --inplace
+# USAGE: cd to directory, run python setup.py build_ext --inplace
+__author__ = "pushpendre"
 from distutils.core import setup
 from distutils.extension import Extension
+from distutils.spawn import find_executable
 from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import os
-os.environ["CC"]="g++ -O3 -DREAL_FLOAT -DPERFORM_NEYSHABUR_MIPS"
-# extra_compile_args=[""],
-# extra_link_args=[""]
-# 
+
+compiler = "g++"
+if find_executable('icpc') is not None:
+    compiler = "icpc"
+COMP_OPT = "%s -fast -DREAL_FLOAT -DPERFORM_NEYSHABUR_MIPS"%compiler
+os.environ["CC"]=COMP_OPT
+os.environ["CXX"]=COMP_OPT
 modules = [Extension("cylsh",
                      ["cylsh.pyx",
                       "lsh.pxd",
@@ -21,8 +26,10 @@ modules = [Extension("cylsh",
                       "SelfTuning.cpp",
                       ],
                      language = "c++",
-                     library_dirs=["/Applications/Canopy.app/appdata/canopy-1.1.0.1371.macosx-x86_64/Canopy.app/Contents/lib"],
                      libraries=["m"],
+                     library_dirs=["/Applications/Canopy.app/appdata/canopy-1.1.0.1371.macosx-x86_64/Canopy.app/Contents/lib"],
+                     # extra_compile_args=[""],
+                     # extra_link_args=[""]
                      )]
 
 for e in modules:
